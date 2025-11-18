3dff2802029c729a0b56d1b2d678543b7604dfa3
António Amândio <antonioamandio22@gmail.com>
2025-11-18 23:31:27 +0100

    updates

---
+++ A_Watermelon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace	std;
+
+int	main(void)
+{
+	int	number;
+
+	cin >> number;
+	if (number % 2 == 0 && number != 2)
+		cout << "YES";
+	else
+		cout << "NO";
+	return (0);
+}
